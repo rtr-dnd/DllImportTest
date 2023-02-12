f3e0a301e0f6ec1aa6d80a87823fe37827c42865
AmaneYamaguchi <yamabach@icloud.com>
2023-02-12 14:41:59 +0900

    initial commit

---
+++ Assets/ImportTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ImportTest : MonoBehaviour
+{
+  [DllImport("NativeDLL")]
+  private static extern int Test(int a, int b);
+  //   [DllImport("InteractionSdk")]
+  //   private static extern int isdk_DataSource_Create(int id);
+  // Start is called before the first frame update
+  void Start()
+  {
+    Debug.Log(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+    int c = Test(1, 2);
+    Debug.Log(c);
+    // var i = isdk_DataSource_Create(0);
+    // Debug.Log(i);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
